2d899a1651520d75cc67cf0e91f0718d1ca32ff6
RafayMemon01 <rafayrashid200611@gmail.com>
2025-01-12 23:30:32 +0500

    fizzbuzz

--- Logic-Building-Practice/index.js
+++ Logic-Building-Practice/index.js
@@ -4,17 +4,36 @@ After gaining a solid understanding of arrays, loops, and if-else statements, I 
 
 // 1- Write a program that checks if a given string or word is a palindrome (a word that reads the same forwards and backwards). Ignore spaces and case sensitivity.
 
-let userResponse = "madam"
+// let userResponse = "madam"
 
-userResponse = userResponse.toLowerCase()
-let filteredResponse = userResponse.split(" ").join("")
-let isPalindrome = false
-for (let i = 0; i < filteredResponse.length; i++) {
-    if(filteredResponse[i] !== filteredResponse[filteredResponse.length - 1 - i]){
-        isPalindrome = false
-        break
+// userResponse = userResponse.toLowerCase()
+// let filteredResponse = userResponse.split(" ").join("")
+// let isPalindrome = false
+// for (let i = 0; i < filteredResponse.length; i++) {
+//     if(filteredResponse[i] !== filteredResponse[filteredResponse.length - 1 - i]){
+//         isPalindrome = false
+//         break
+//     }
+//     isPalindrome = true
+
+// }
+// console.log(isPalindrome);
+
+// 2- Write a program that prints numbers from 1 to 100. For each number:
+    // Print "Fizz" if it’s divisible by 3,
+    // Print "Buzz" if it’s divisible by 5,
+    // Print "FizzBuzz" if it’s divisible by both 3 and 5,
+    // Otherwise, print the number itself.
+
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("fizzBuzz");
+    } else if (i % 3 === 0) {
+        console.log("fizz");
+    } else if (i % 5 === 0) {
+        console.log("buzz");
+    } else {
+        console.log(i)
     }
-    isPalindrome = true
-        
 }
-console.log(isPalindrome);
