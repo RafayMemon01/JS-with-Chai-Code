40deeeaa920d54d1cd3338b601e93631668006a8
RafayMemon01 <rafayrashid200611@gmail.com>
2025-01-13 21:07:24 +0500

    third task heading

--- Logic-Building-Practice/index.js
+++ Logic-Building-Practice/index.js
@@ -26,14 +26,23 @@ After gaining a solid understanding of arrays, loops, and if-else statements, I 
     // Otherwise, print the number itself.
 
 
-for (let i = 1; i <= 100; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-        console.log("fizzBuzz");
-    } else if (i % 3 === 0) {
-        console.log("fizz");
-    } else if (i % 5 === 0) {
-        console.log("buzz");
-    } else {
-        console.log(i)
-    }
-}
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//         console.log("fizzBuzz");
+//     } else if (i % 3 === 0) {
+//         console.log("fizz");
+//     } else if (i % 5 === 0) {
+//         console.log("buzz");
+//     } else {
+//         console.log(i)
+//     }
+// }
+
+// 3- Write a program that checks if a given number is prime. A prime number is a number greater than 1 that cannot be formed by multiplying two smaller natural numbers.
+
+    // How to do it:
+    // Use a loop to check if the given number is divisible by any number other than 1 and itself.
+    // If the number is divisible by any other number, it’s not prime.
+    // If no divisors are found, it is a prime number.
+
+
