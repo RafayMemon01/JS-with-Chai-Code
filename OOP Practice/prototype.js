415616be4255766605aa8146579fceace1c6595f
RafayMemon01 <rafayrashid200611@gmail.com>
2025-01-19 23:01:33 +0500

    Code

---
+++ OOP Practice/prototype.js
@@ -0,0 +1,15 @@
+let computer = {
+    cpu: 5,
+    ram: 32,
+    gpu: 5090,
+    nvme: "4 TB"
+}
+
+let lenovo = {
+    screen: "HD",
+    
+}
+
+lenovo.abc= computer
+
+console.log( `lenovo`, lenovo.cpu )
